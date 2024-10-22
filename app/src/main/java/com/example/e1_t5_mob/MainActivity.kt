3936bf2b7d89b8bf510fd0f2f6e2b0951d32775a
Memelero <124786659+Memelero@users.noreply.github.com>
2024-10-22 11:55:55 +0200

    FUNCIONAA!!!

--- app/src/main/java/com/example/e1_t5_mob/MainActivity.kt
+++ app/src/main/java/com/example/e1_t5_mob/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.e1_t5_mob
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnRegistro = findViewById<Button>(R.id.btnRegistro)
+        btnRegistro.setOnClickListener {
+            //cambiar de ventana
+            val intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
         }
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            //cambiar de ventana
+            val intent2 = Intent(this, LoginActivity::class.java)
+            startActivity(intent2)
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/e1_t5_mob/RegistroActivity.kt
@@ -0,0 +1,14 @@
+package com.example.e1_t5_mob
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegistroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+
+
+
+    }
+}
